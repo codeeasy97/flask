329745b3ebb4862625c4b05c4f9c8c9a3f93f7e2
dcpsquare <deepak.pandey@thesqua.re>
2022-11-23 23:03:15 +0530

    introduction

---
+++ 01-Introduction/introduction.py
@@ -0,0 +1,22 @@
+"""
+Introduction :
+
+Python is a popular programming language. It was created by Guido van Rossum, 
+and released in 1991.
+
+    • web development (server-side)
+    • software development (Desktop Application)
+    • scripting
+    • Machine Learning.
+    • Artificial Intelligence
+    • Big Data (Extract, Load, Transform)
+
+Why Choose Python?
+    • Python works on different platforms (Windows, Mac, Linux, Raspberry Pi, etc).
+    • Python has a simple syntax
+    • Python can be treated in a procedural way, an object-oriented way or a functional way
+    • Object Oriented
+    • Open Source
+    • Interpreted
+
+"""
