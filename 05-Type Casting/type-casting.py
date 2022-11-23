5c61b1245e09083592c9fbbdfaccca2def8cfa2c
dcpsquare <deepak.pandey@thesqua.re>
2022-11-23 23:06:53 +0530

    type casting

---
+++ 05-Type Casting/type-casting.py
@@ -0,0 +1,25 @@
+a = 123.20
+print(type(a))
+
+b= int(a)
+print(type(b))
+
+a = 1.2
+b= 2
+c = a+b
+print(type(c))
+
+a = 9
+b = str(a)
+
+print(type(b))
+
+a = '3'
+b = int(a)
+print(type(b))
+
+a = "4.5"
+
+b= float(a)
+
+print(type(b))
