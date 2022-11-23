5dc45b4887abc783da357855c8a43c69a503297e
dcpsquare <deepak.pandey@thesqua.re>
2022-11-23 23:04:33 +0530

    python command line

---
+++ 02-python-cli/python-cli.py
@@ -0,0 +1,30 @@
+"""
+Open Your Terminal
+
+1.  type python and Press Enter , Now you are into python command Line
+
+2. Type : Print("Hello World!")
+    Output : Hello World
+
+3.  Add Two Number
+    3+5 Press Enter
+    8
+
+4. Add Two Number Using Variable
+    a = 3 (Press Enter)
+    b = 5   (Press Enter)
+    c = a+b (Press Enter)
+    c   (Press Enter)
+    output : 8
+
+5. Multiply Two Number Using variable
+    a = 3 (Press Enter)
+    b = 5   (Press Enter)
+    c = a*b (Press Enter)
+    c   (Press Enter)
+    output : 15
+
+6. How to Run Python Script
+    python first.py
+
+"""
