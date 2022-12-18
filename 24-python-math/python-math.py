56bce7100e9a8f19618dfaaa6bca148d381b68c0
dcpsquare <deepak.pandey@thesqua.re>
2022-12-18 22:22:32 +0530

    mathematical function in python

---
+++ 24-python-math/python-math.py
@@ -0,0 +1,39 @@
+# Python Math
+
+#Built-in Math Functions
+x = min(1,2,3,4)
+print(x)
+
+x = max(1,2,3,4)
+print(x)
+
+x = abs(-9.73)
+print(x)
+
+x = pow(2,3)
+print(x)
+#The Math Module
+
+import math
+
+
+x = math.ceil(1.67)
+print(x)
+
+x = math.floor(1.67)
+print(x)
+
+x = math.sqrt(64)
+print(x)
+
+x = math.pi
+print(x)
+
+x = math.tan(45)
+print(x)
+
+x = math.cos(90)
+print(x)
+
+x = math.sin(30)
+print(x)
