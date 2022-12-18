21fef035b60fff6c844bf1da99abe52f812b65d0
dcpsquare <deepak.pandey@thesqua.re>
2022-12-18 13:38:38 +0530

    python modules

---
+++ 22-modules/modules.py
@@ -0,0 +1,29 @@
+# Python Modules
+
+#What is a Module?
+
+def myCodeModule(name):
+    return "Hello ", name
+
+
+names = {
+    "name": "pawan",
+    "age": 27
+}
+
+
+mystr = "hello"
+#Create a Module
+
+#Use a Module
+
+#Variables in Module
+
+#Naming a Module
+
+
+#Built-in Modules
+
+#Using the dir() Function
+
+#Import From Module
---
+++ 22-modules/name.py
@@ -0,0 +1,21 @@
+# from modules import myCodeModule, names, mystr
+
+# x = myCodeModule("Ram")
+# print(x[0],x[1])
+
+# x = names["name"]
+# print(x)
+
+# print(mystr)
+
+import modules as m
+print(m.names)
+
+import platform
+
+print(dir(platform))
+
+print(platform.system())
+print(platform.python_version())
+
+print(dir(m))
